3f85d677d679f3e1aae3d42d058e88930f6a365d
Vikasrathore2007 <vikasrathore21752@gmail.com>
2026-05-30 16:17:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// if else else if
+//switch case
+// early return
+
+
+
+//loops it is defined as repitiiton of something again and again 
+// type -> for, while,do while
+//for
+//kaha se jana hai-> kaha tak jana hai-> kaise jana hai
+//while
+// kaha se jana hai->kab rukna hai->kaise rukna hai
+//for(variabletype Start;end;change){ what do to if true}
+//for(let i=0;i<50;i++){console.log(i);}
+
+
+
+//while(end){what to do after meeting the condition}
+let i=41;
+while(i<44){console.log(i);
+
+}
